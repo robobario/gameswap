2c35a47616744e753df825225dada644ecdc0c18
Mark Tokumaru <mark.tokumaru@adscale.co.nz>
2016-02-02 16:45:11 +1300

    Add placeholder account views

--- gameswap-service/src/main/resources/assets/app/js/app.js
+++ gameswap-service/src/main/resources/assets/app/js/app.js
@@ -1,9 +1,62 @@
 // Declare app level module which depends on filters, and services
-angular.module('gameswap', ['ngResource', 'ngRoute', 'ui.bootstrap', 'ui.date'])
-  .config(['$routeProvider', function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/home/home.html', 
-        controller: 'HomeController'})
-      .otherwise({redirectTo: '/'});
-  }]);
+angular.module('gameswap', ['ngResource', 'ui.bootstrap', 'ui.date', 'ui.router'])
+
+    .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('home', {
+                url: '/',
+                controller: 'HomeController',
+                templateUrl: 'views/home/home.html'
+            })
+            .state('login', {
+                url: '/login',
+                templateUrl: 'views/partials/login.html',
+                controller: 'LoginCtrl',
+                resolve: {
+                    skipIfLoggedIn: skipIfLoggedIn
+                }
+            })
+            .state('signup', {
+                url: '/signup',
+                templateUrl: 'views/partials/signup.html',
+                controller: 'SignupCtrl',
+                resolve: {
+                    skipIfLoggedIn: skipIfLoggedIn
+                }
+            })
+            .state('logout', {
+                url: '/logout',
+                template: null,
+                controller: 'LogoutCtrl'
+            })
+            .state('profile', {
+                url: '/profile',
+                templateUrl: 'views/partials/profile.html',
+                controller: 'ProfileCtrl',
+                resolve: {
+                    loginRequired: loginRequired
+                }
+            });
+
+        $urlRouterProvider.otherwise('/');
+
+        function skipIfLoggedIn($q, $auth) {
+            var deferred = $q.defer();
+            if ($auth.isAuthenticated()) {
+                deferred.reject();
+            } else {
+                deferred.resolve();
+            }
+            return deferred.promise;
+        }
+
+        function loginRequired($q, $location, $auth) {
+            var deferred = $q.defer();
+            if ($auth.isAuthenticated()) {
+                deferred.resolve();
+            } else {
+                $location.path('/login');
+            }
+            return deferred.promise;
+        }
+    }]);
